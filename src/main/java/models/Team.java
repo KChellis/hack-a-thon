f6a94c7c3d36aef196f3077bafbad705d8ef6545
Kristen Chellis <peanutster@gmail.com>
2018-06-01 08:51:13 -0700

    test that new team instantiates correctly

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String name;
+    private String description;
+    private List<String> members = new ArrayList<>();
+
+    public Team(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TeamTest {
 
+    @Test
+    public void newTeam_instantiatesCorrectly_true() {
+        Team testTeam = new Team("Epicodus", "A group of Java Students");
+        assertTrue(testTeam instanceof Team);
+    }
 }
