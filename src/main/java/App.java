01662611709f354f7684102fd5645f4503e4f819
Kristen Chellis <peanutster@gmail.com>
2018-06-01 10:12:38 -0700

    add team list page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,15 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView (model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/teams", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("teams", Team.getAll());
+            return new ModelAndView(model, "teams.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
